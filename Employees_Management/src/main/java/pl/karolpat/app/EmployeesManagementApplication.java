0ad31ea640d567f81aadf96a6ce4fc47e0780363
karolpat <karol.patecki09@gmail.com>
2018-03-15 11:59:42 +0100

    Project init

---
+++ Employees_Management/src/main/java/pl/karolpat/app/EmployeesManagementApplication.java
@@ -0,0 +1,12 @@
+package pl.karolpat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesManagementApplication.class, args);
+	}
+}
