655bc17ca09f36315b32f7805baf78d2d73c2b2e
karolpat <karol.patecki09@gmail.com>
2018-03-20 00:05:33 +0100

    All methods implemented. Pagination still TODO

--- Employees_Management/src/main/java/pl/karolpat/controller/EmployeeController.java
+++ Employees_Management/src/main/java/pl/karolpat/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,69 +15,131 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.karolpat.entity.Employee;
+import pl.karolpat.entity.Position;
+import pl.karolpat.exception.EmployeeNotFoundException;
 import pl.karolpat.exception.ErrorRespone;
-import pl.karolpat.exception.InstanceNotFound;
+import pl.karolpat.exception.NonuniqueEmailException;
+import pl.karolpat.exception.PositionNotFoundException;
 import pl.karolpat.service.EmployeeService;
+import pl.karolpat.service.PositionService;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	
 	private EmployeeService employeeService;
-	
-	public EmployeeController(EmployeeService employeeService) {
-		this.employeeService = employeeService; 
+	private PositionService positionService;
+
+	public EmployeeController(EmployeeService employeeService, PositionService positionService) {
+		this.employeeService = employeeService;
+		this.positionService = positionService;
 	}
-	
-	@ExceptionHandler(InstanceNotFound.class)
-	public ResponseEntity<ErrorRespone> handleException(Exception e) {
+
+	/**
+	 * Exception handling in case searching for the Employee by given id results as
+	 * null.
+	 * 
+	 * @param e
+	 *            - Exception thrown by any method.
+	 * @return HttpStatus number and a message.
+	 */
+	@ExceptionHandler(EmployeeNotFoundException.class)
+	public ResponseEntity<ErrorRespone> handlePreconditionException(Exception e) {
 		ErrorRespone error = new ErrorRespone();
 		error.setMessage(e.getMessage());
 		error.setErrorCode(HttpStatus.PRECONDITION_FAILED.value());
 		return new ResponseEntity<ErrorRespone>(error, HttpStatus.OK);
-		
+
 	}
-	
+
+	/**
+	 * Exception handling in case an internal server error occurs. It would be
+	 * caused by given email that is nonunique or position id that does not exist.
+	 * 
+	 * @param e
+	 *            - Exception thrown by any method.
+	 * @return HttpStatus number and a message.
+	 */
+	@ExceptionHandler({ PositionNotFoundException.class, NonuniqueEmailException.class })
+	public ResponseEntity<ErrorRespone> handleServerErrorException(Exception e) {
+		ErrorRespone error = new ErrorRespone();
+		error.setMessage(e.getMessage());
+		error.setErrorCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return new ResponseEntity<ErrorRespone>(error, HttpStatus.OK);
+	}
+
 	@GetMapping("")
 	@ResponseBody
-	public List<Employee> getAllActive(){
+	public List<Employee> getAllActive() {
 		return employeeService.getAllActive();
 	}
-	
+
 	@GetMapping("/withDeleted")
 	@ResponseBody
-	public List<Employee> getAll(){
+	public List<Employee> getAll() {
 		return employeeService.getAll();
 	}
-	
+
 	@GetMapping("/firstName/{firstName}")
 	@ResponseBody
 	public List<Employee> employeesByFirstName(@PathVariable("firstName") String firstName) {
 		return employeeService.getAllByFirstName(firstName);
 	}
-	
+
 	@GetMapping("/lastName/{lastName}")
 	@ResponseBody
 	public List<Employee> employeesByLastName(@PathVariable("lastName") String lastName) {
 		return employeeService.getAllByLastName(lastName);
 	}
-	
+
 	@GetMapping("/email/{email:.+}")
 	@ResponseBody
 	public Employee employeeByemail(@PathVariable("email") String email) {
 		return employeeService.getOneByEmail(email);
 	}
-	
+
+	/**
+	 * Changing employee active status to false. Thanks to that, such employee will
+	 * not be present in searching for employees by first name, last name or email
+	 * but remains in the database and is easy to restore. No employee disappears.
+	 * 
+	 * @param id
+	 *            id of employee to delete (hide in fact).
+	 * @return Deleted employee.
+	 * @throws EmployeeNotFoundException
+	 *             in case there is no employee with given id.
+	 */
 	@PutMapping("/delete/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public Employee deleteEmployee(@PathVariable("id") long id) throws InstanceNotFound {
+	public Employee deleteEmployee(@PathVariable("id") long id) throws EmployeeNotFoundException {
 		return employeeService.removeEmployee(id);
 	}
-	
+
+	/**
+	 * Changing employee active status to true so that this employee will be present
+	 * in searching for employees by first name, last name or email again.
+	 * 
+	 * @param id
+	 *            id of employee to be restored.
+	 * @return Restored employee.
+	 * @throws EmployeeNotFoundException
+	 *             in case there is no employee with given id.
+	 */
 	@PutMapping("/restore/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public Employee restoreEmployee(@PathVariable("id") long id) throws InstanceNotFound {
+	public Employee restoreEmployee(@PathVariable("id") long id) throws EmployeeNotFoundException {
 		return employeeService.restoreEmployee(id);
+	}
+
+	@PostMapping("/add/{firstName}/{lastName}/{position}/{email:.+}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Employee addNewEmployee(@PathVariable("firstName") String firstName,
+			@PathVariable("lastName") String lastName,
+			@PathVariable("position") long position,
+			@PathVariable("email") String email)
+					throws PositionNotFoundException, NonuniqueEmailException {
+
+		Position pos = positionService.findOneById(position);
+		return employeeService.addEmployee(firstName, lastName, pos, email);
 	}
 }
