9f5f65bd205053ca029dc75d67fe7f762d9d4e06
karolpat <karol.patecki09@gmail.com>
2018-03-19 22:22:59 +0100

    Employee and Position Repo

---
+++ Employees_Management/src/main/java/pl/karolpat/repository/EmployeeRepo.java
@@ -0,0 +1,18 @@
+package pl.karolpat.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.karolpat.entity.Employee;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+	List<Employee> findAllByFirstNameAndActive(String firstName, boolean active);
+
+	List<Employee> findAllByLastNameAndActive(String lastName, boolean active);
+	
+	List<Employee> findAllByActive(boolean active);
+
+	Employee findOneByEmailAndActive(String email, boolean active);
+}
---
+++ Employees_Management/src/main/java/pl/karolpat/repository/PositionRepo.java
@@ -0,0 +1,9 @@
+package pl.karolpat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.karolpat.entity.Position;
+
+public interface PositionRepo extends JpaRepository<Position, Long> {
+
+}
