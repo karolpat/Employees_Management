1801666e6a91eedaae48261ea02f0d3425def11c
karolpat <karol.patecki09@gmail.com>
2018-03-19 23:55:27 +0100

    More insertions. Added position insertions

--- Employees_Management/src/main/resources/data.sql
+++ Employees_Management/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into employee (first_name, last_name, email) values('Karol','Pat', 'karol@Op.pl'),('Karol','Pate', 'karo@op.pl'), ('Karol', 'kkk', 'kkk@op.pl'); 
+insert into position (name) values ('Sales'), ('Management'), ('Interns'), ('Finance'),('Administration');
+
+insert into employee (first_name, last_name, email,position_id) values('Karol','Patecki', 'karol@karol.pl',2),('Michał','Kowalski', 'michal@michal.pl',2), ('Michał', 'Jankowski', 'michal@jankowski.pl',3),('Jan', 'Kowalski', 'jan@jankowski.pl',1); 
