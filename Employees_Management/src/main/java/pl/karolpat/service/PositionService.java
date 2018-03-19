95b78e64269fe282214b666af1bf89b07617d339
karolpat <karol.patecki09@gmail.com>
2018-03-20 00:09:11 +0100

    Searching for the position by ID

--- Employees_Management/src/main/java/pl/karolpat/service/PositionService.java
+++ Employees_Management/src/main/java/pl/karolpat/service/PositionService.java
@@ -1,5 +1,9 @@
 package pl.karolpat.service;
 
+import pl.karolpat.entity.Position;
+import pl.karolpat.exception.PositionNotFoundException;
+
 public interface PositionService {
 
+	Position findOneById(long id) throws PositionNotFoundException;
 }
