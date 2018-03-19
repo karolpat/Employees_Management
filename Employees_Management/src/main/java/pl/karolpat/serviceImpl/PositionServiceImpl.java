dcac6483c5a9fffdadabacf70a917d47c4db5d7d
karolpat <karol.patecki09@gmail.com>
2018-03-20 00:10:00 +0100

    Searching for the position by ID

--- Employees_Management/src/main/java/pl/karolpat/serviceImpl/PositionServiceImpl.java
+++ Employees_Management/src/main/java/pl/karolpat/serviceImpl/PositionServiceImpl.java
@@ -1,7 +1,32 @@
 package pl.karolpat.serviceImpl;
 
-import pl.karolpat.service.PositionService;
+import org.springframework.stereotype.Service;
 
+import pl.karolpat.entity.Position;
+import pl.karolpat.exception.PositionNotFoundException;
+import pl.karolpat.repository.PositionRepo;
+import pl.karolpat.service.PositionService;
+@Service
 public class PositionServiceImpl implements PositionService {
+
+	private static final String EXCEPTION_MESSAGE = "There is no position with a such ID.";
+
+	private PositionRepo positionRepo;
+
+	public PositionServiceImpl(PositionRepo positionRepo) {
+		this.positionRepo = positionRepo;
+	}
+
+	@Override
+	public Position findOneById(long id) throws PositionNotFoundException {
+
+		Position position = positionRepo.findOne(id);
+		if (position == null) {
+			throw new PositionNotFoundException(EXCEPTION_MESSAGE);
+		} else {
+			return position;
+		}
+
+	}
 
 }
